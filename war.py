6c2079d2e218256c8746f1ace69c92469b54056b
real-currywa <korewaoggar@gmail.com>
2026-03-21 17:04:50 +0100

    save

--- war.py
+++ war.py
@@ -3,6 +3,5 @@ import streamlit as st
 import webbrowser
 st.header('Gbolade')
 st.write("war thunder tips")
-x = url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 if st.button('open video'):
-    webbrowser.open(x)
+    st.write("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
