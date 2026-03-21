2c72c68e3ba2496d148009bb7f0c80f273bae6dc
real-currywa <korewaoggar@gmail.com>
2026-03-21 16:45:04 +0100

    save

--- war.py
+++ war.py
@@ -4,5 +4,4 @@ import webbrowser
 st.header('Gbolade')
 st.write("war thunder tips")
 url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-if st.button('open video'):
-    webbrowser.open(url)
+webbrowser.open(url)
